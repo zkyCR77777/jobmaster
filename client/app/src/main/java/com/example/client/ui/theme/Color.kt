818921cc3e7fe3ad0e0d984a1b3fc6f811a2fe57
zky <2293852663@qq.com>
2026-03-15 19:39:11 +0800

    爬虫实现

--- client/app/src/main/java/com/example/client/ui/theme/Color.kt
+++ client/app/src/main/java/com/example/client/ui/theme/Color.kt
@@ -2,9 +2,23 @@ package com.example.client.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Warm Neutrals (Stone)
+val Stone50 = Color(0xFFFAF9F6)
+val Stone100 = Color(0xFFF5F5F4)
+val Stone200 = Color(0xFFE7E5E4)
+val Stone300 = Color(0xFFD6D3D1)
+val Stone400 = Color(0xFFA8A29E)
+val Stone500 = Color(0xFF78716C)
+val Stone600 = Color(0xFF57534E)
+val Stone700 = Color(0xFF44403C)
+val Stone800 = Color(0xFF292524)
+val Stone900 = Color(0xFF1C1917)
+
+val White = Color(0xFFFFFFFF)
+
+// Slate
 val Slate50 = Color(0xFFF8FAFC)
 val Slate100 = Color(0xFFF1F5F9)
-val Slate150 = Color(0xFFE8EEF6)
 val Slate200 = Color(0xFFE2E8F0)
 val Slate300 = Color(0xFFCBD5E1)
 val Slate400 = Color(0xFF94A3B8)
@@ -14,35 +28,18 @@ val Slate700 = Color(0xFF334155)
 val Slate800 = Color(0xFF1E293B)
 val Slate900 = Color(0xFF0F172A)
 
-val White = Color(0xFFFFFFFF)
-val Indigo50 = Color(0xFFEEF2FF)
-val Indigo100 = Color(0xFFE0E7FF)
-val Indigo500 = Color(0xFF6366F1)
-val Indigo600 = Color(0xFF4F46E5)
-val Blue50 = Color(0xFFEFF6FF)
-val Blue100 = Color(0xFFDBEAFE)
-val Blue400 = Color(0xFF60A5FA)
-val Blue500 = Color(0xFF3B82F6)
-val Blue600 = Color(0xFF2563EB)
-val Cyan400 = Color(0xFF22D3EE)
-
-val Pink50 = Color(0xFFFDF2F8)
-val Pink100 = Color(0xFFFCE7F3)
-val Pink500 = Color(0xFFEC4899)
-val Pink600 = Color(0xFFDB2777)
-val Rose400 = Color(0xFFFB7185)
-
-val Emerald50 = Color(0xFFECFDF5)
-val Emerald100 = Color(0xFFD1FAE5)
-val Emerald500 = Color(0xFF10B981)
-val Emerald600 = Color(0xFF059669)
-val Teal400 = Color(0xFF2DD4BF)
+// Warm Primaries
+val Orange50 = Color(0xFFFFF7ED)
+val Orange100 = Color(0xFFFFEDD5)
+val Orange400 = Color(0xFFFB923C)
+val Orange500 = Color(0xFFF97316)
+val Orange600 = Color(0xFFEA580C)
+val Orange700 = Color(0xFFC2410C)
 
 val Amber50 = Color(0xFFFFFBEB)
 val Amber100 = Color(0xFFFEF3C7)
 val Amber500 = Color(0xFFF59E0B)
 val Amber600 = Color(0xFFD97706)
-val Orange400 = Color(0xFFFB923C)
 
 val Red50 = Color(0xFFFEF2F2)
 val Red100 = Color(0xFFFEE2E2)
@@ -50,15 +47,45 @@ val Red400 = Color(0xFFF87171)
 val Red500 = Color(0xFFEF4444)
 val Red600 = Color(0xFFDC2626)
 
-val AppBackground = Color(0xFFFBFCFE)
+// Cool colors
+val Blue50 = Color(0xFFEFF6FF)
+val Blue100 = Color(0xFFDBEAFE)
+val Blue400 = Color(0xFF60A5FA)
+val Blue500 = Color(0xFF3B82F6)
+val Blue600 = Color(0xFF2563EB)
+
+val Cyan400 = Color(0xFF22D3EE)
+
+val Emerald50 = Color(0xFFECFDF5)
+val Emerald100 = Color(0xFFD1FAE5)
+val Emerald500 = Color(0xFF10B981)
+val Emerald600 = Color(0xFF059669)
+
+val Teal400 = Color(0xFF2DD4BF)
+val Teal500 = Color(0xFF14B8A6)
+
+val Pink50 = Color(0xFFFDF2F8)
+val Pink100 = Color(0xFFFCE7F3)
+val Pink500 = Color(0xFFEC4899)
+val Pink600 = Color(0xFFDB2777)
+
+val Rose400 = Color(0xFFFB7185)
+
+val Indigo50 = Color(0xFFEEF2FF)
+val Indigo100 = Color(0xFFE0E7FF)
+val Indigo500 = Color(0xFF6366F1)
+val Indigo600 = Color(0xFF4F46E5)
+
+// Functional Colors
+val AppBackground = Stone50
 val AppSurface = White
-val AppBorder = Color(0xFFE6ECF5)
-val AppBorderStrong = Color(0xFFD8E2EF)
-val AppTextPrimary = Slate900
-val AppTextSecondary = Slate500
-val AppTextMuted = Slate400
-val AppPrimary = Indigo600
-val AppPrimarySoft = Indigo50
-val AppSuccess = Emerald600
+val AppBorder = Stone200
+val AppBorderStrong = Stone300
+val AppTextPrimary = Stone900
+val AppTextSecondary = Stone600
+val AppTextMuted = Stone400
+val AppPrimary = Orange600
+val AppPrimarySoft = Orange50
+val AppSuccess = Color(0xFF16A34A) // A warmer green
 val AppWarning = Amber600
 val AppDanger = Red600
